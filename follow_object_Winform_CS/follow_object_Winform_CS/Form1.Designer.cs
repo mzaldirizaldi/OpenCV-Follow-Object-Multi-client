35aaa7c06de9c4c5b716057421b9ba479edb1407
Aufa R <aufarandy18@gmail.com>
2022-10-07 17:01:58 +0700

    Create C# Winform

---
+++ follow_object_Winform_CS/follow_object_Winform_CS/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace follow_object_Winform_CS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblElevasi = new System.Windows.Forms.Label();
+            this.lblAzimuth = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblY = new System.Windows.Forms.Label();
+            this.lblX = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.tbPort = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbIP = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(111)))), ((int)(((byte)(34)))), ((int)(((byte)(50)))));
+            this.groupBox2.Controls.Add(this.lblElevasi);
+            this.groupBox2.Controls.Add(this.lblAzimuth);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
+            this.groupBox2.ForeColor = System.Drawing.Color.White;
+            this.groupBox2.Location = new System.Drawing.Point(302, 88);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(275, 113);
+            this.groupBox2.TabIndex = 24;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Posisi Servo";
+            // 
+            // lblElevasi
+            // 
+            this.lblElevasi.AutoSize = true;
+            this.lblElevasi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblElevasi.ForeColor = System.Drawing.Color.White;
+            this.lblElevasi.Location = new System.Drawing.Point(78, 69);
+            this.lblElevasi.Name = "lblElevasi";
+            this.lblElevasi.Size = new System.Drawing.Size(20, 16);
+            this.lblElevasi.TabIndex = 6;
+            this.lblElevasi.Text = "0°";
+            // 
+            // lblAzimuth
+            // 
+            this.lblAzimuth.AutoSize = true;
+            this.lblAzimuth.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.lblAzimuth.ForeColor = System.Drawing.Color.White;
+            this.lblAzimuth.Location = new System.Drawing.Point(78, 31);
+            this.lblAzimuth.Name = "lblAzimuth";
+            this.lblAzimuth.Size = new System.Drawing.Size(24, 17);
+            this.lblAzimuth.TabIndex = 5;
+            this.lblAzimuth.Text = "0°";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(6, 31);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 17);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Azimuth :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(11, 69);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 17);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Elevasi :";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(111)))), ((int)(((byte)(34)))), ((int)(((byte)(50)))));
+            this.groupBox1.Controls.Add(this.lblY);
+            this.groupBox1.Controls.Add(this.lblX);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
+            this.groupBox1.ForeColor = System.Drawing.Color.White;
+            this.groupBox1.Location = new System.Drawing.Point(21, 88);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(275, 113);
+            this.groupBox1.TabIndex = 23;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Posisi Objek";
+            // 
+            // lblY
+            // 
+            this.lblY.AutoSize = true;
+            this.lblY.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.lblY.ForeColor = System.Drawing.Color.White;
+            this.lblY.Location = new System.Drawing.Point(78, 69);
+            this.lblY.Name = "lblY";
+            this.lblY.Size = new System.Drawing.Size(17, 17);
+            this.lblY.TabIndex = 4;
+            this.lblY.Text = "0";
+            // 
+            // lblX
+            // 
+            this.lblX.AutoSize = true;
+            this.lblX.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.lblX.ForeColor = System.Drawing.Color.White;
+            this.lblX.Location = new System.Drawing.Point(78, 31);
+            this.lblX.Name = "lblX";
+            this.lblX.Size = new System.Drawing.Size(17, 17);
+            this.lblX.TabIndex = 3;
+            this.lblX.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(6, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Posisi X :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(7, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Posisi Y :";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnStop.Location = new System.Drawing.Point(216, 36);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(80, 26);
+            this.btnStop.TabIndex = 22;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // tbPort
+            // 
+            this.tbPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.tbPort.Location = new System.Drawing.Point(105, 38);
+            this.tbPort.Name = "tbPort";
+            this.tbPort.Size = new System.Drawing.Size(100, 23);
+            this.tbPort.TabIndex = 21;
+            this.tbPort.Text = "9999";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(54, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 17);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Port";
+            // 
+            // tbIP
+            // 
+            this.tbIP.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.tbIP.Location = new System.Drawing.Point(105, 12);
+            this.tbIP.Name = "tbIP";
+            this.tbIP.Size = new System.Drawing.Size(100, 23);
+            this.tbIP.TabIndex = 19;
+            this.tbIP.Text = "192.168.18.229";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 17);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "IP Address";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnStart.Location = new System.Drawing.Point(216, 10);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(80, 26);
+            this.btnStart.TabIndex = 17;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(195)))), ((int)(((byte)(7)))), ((int)(((byte)(63)))));
+            this.ClientSize = new System.Drawing.Size(597, 220);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.tbPort);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbIP);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Follow Object";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lblElevasi;
+        private System.Windows.Forms.Label lblAzimuth;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblY;
+        private System.Windows.Forms.Label lblX;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.TextBox tbPort;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbIP;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ follow_object_Winform_CS/follow_object_Winform_CS/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using System.Windows.Forms;
+using SimpleTCP;
+
+namespace follow_object_Winform_CS
+{
+    public partial class Form1 : Form
+    {
+        private object syncGate = new object();
+        private Process process;
+        private StringBuilder output = new StringBuilder();
+        private bool outputChanged;
+        SimpleTcpClient client;
+        string DataIn;
+        sbyte indexOfB;
+        sbyte indexOfA;
+        sbyte indexOfX;
+        sbyte indexOfY;
+        int StarY;
+        int StarA;
+        int StarB;
+        string ser1, ser2, ser3, ser4;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            System.Net.IPAddress ip = System.Net.IPAddress.Parse(tbIP.Text);
+            client.Connect(tbIP.Text, Convert.ToInt32(tbPort.Text));
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lock (syncGate)
+            {
+                if (process != null) return;
+            }
+
+            client = new SimpleTcpClient();
+            client.Delimiter = 0x13;//enter
+            client.StringEncoder = Encoding.UTF8;
+            client.DataReceived += Servo_DataReceived;
+            DataIn = "";
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+        }
+
+        private void OnOutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            lock (syncGate)
+            {
+                if (sender != process) return;
+                output.AppendLine(e.Data);
+                if (outputChanged) return;
+                outputChanged = true;
+                BeginInvoke(new Action(OnOutputChanged));
+            }
+        }
+
+        private void OnOutputChanged()
+        {
+            lock (syncGate)
+            {
+                //richTextBox1.Text = output.ToString();
+                outputChanged = false;
+            }
+        }
+
+        private void OnProcessExited(object sender, EventArgs e)
+        {
+            lock (syncGate)
+            {
+                if (sender != process) return;
+                process.Dispose();
+                process = null;
+            }
+        }
+
+        private void Servo_DataReceived(object sender, SimpleTCP.Message e)
+        {
+
+            lblX.Invoke((MethodInvoker)delegate ()
+            {
+                DataIn = e.MessageString;
+                if (DataIn.Length > 5)
+                {
+                    //tbX.Text = DataIn;
+                    indexOfX = Convert.ToSByte(DataIn.IndexOf("X"));
+                    ser1 = DataIn.Substring(0, indexOfX);
+                    lblX.Text = ser1;
+
+                    StarY = indexOfX + 1;
+                    indexOfY = Convert.ToSByte(DataIn.IndexOf("Y"));
+                    ser2 = DataIn.Substring(StarY, indexOfY - StarY);
+                    lblY.Text = ser2;
+
+                    StarA = indexOfY + 1;
+                    indexOfA = Convert.ToSByte(DataIn.IndexOf("A"));
+                    ser3 = DataIn.Substring(StarA, indexOfA - StarA);
+                    lblAzimuth.Text = ser3 + "°";
+
+                    StarB = indexOfA + 1;
+                    indexOfB = Convert.ToSByte(DataIn.IndexOf("B"));
+                    ser4 = DataIn.Substring(StarB, indexOfB - StarB);
+                    lblElevasi.Text = ser4 + "°";
+                }
+            });
+        }
+    }
+}
